f95ef5bddc74eb19153e8389d3d1c06f8af33fe9
Nicolas Le Gal <nl20121974@hotmail.com>
2023-03-31 22:20:20 +0200

    Ajoutez des fichiers projet.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CC.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public const string HubUrl = "/chat";
+
+        public async Task Broadcast(string username, string message)
+        {
+            await Clients.All.SendAsync("Broadcast", username, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"{Context.ConnectionId} connected");
+            return base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception e)
+        {
+            Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
+            if (e != null)
+                await base.OnDisconnectedAsync(e);
+        }
+
+        public Task SendPrivateMessage(string user, string message)
+        {
+            return Clients.User(user).SendAsync("ReceiveMessage", message);
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
+
+        public string GetConnectionId()
+        {
+            return Context.ConnectionId;
+        }
+    }
+}
